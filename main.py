833b9711a2ce1f361540980284abc2b62312cdbb
Canek <coeus@hestia.local>
2025-10-14 18:16:11 -0700

    Adding player drawing and movement

--- main.py
+++ main.py
@@ -1,25 +1,45 @@
+import sys
 import pygame
-from constants import *
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, BACKGROUND_COLOR
 from player import Player
 
+
 def main():
-  pygame.init()
-  print("Starting Sneak!")
-  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.init()
+    print("Starting Sneak!")
+    print(f'Screen width: {SCREEN_WIDTH}')
+    print(f'Screen height: {SCREEN_HEIGHT}')
 
-  drawable = pygame.sprite.Group()
-  Player.containers = (drawable)
-  player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
-  while True:
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        return
-    for item in drawable:
-      item.draw(screen)
-    
-    screen.fill(BACKGROUND_COLOR)
-    pygame.display.flip()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill(BACKGROUND_COLOR)
+        updatable.update(dt)
+
+        if player_1.collides_with_border(SCREEN_WIDTH, SCREEN_HEIGHT):
+            print("Game Over!")
+            sys.exit()
+
+        # print(f'Player position: {player_1.position}')
+
+        for item in drawable:
+            item.draw(screen)
+
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
-  main()
+    main()
--- player.py
+++ player.py
@@ -1,17 +1,37 @@
 import pygame
-from constants import PLAYER_WIDTH
+from constants import PLAYER_WIDTH, INITIAL_PLAYER_SPEED
 from squareshape import SquareShape
 
-class Player(SquareShape):
-  def __init__(self, x, y):
-    super().__init__(x, y)
-    
-  def draw(self, screen):
-    pygame.draw.polygon(screen, (0, 0, 0), self.square(), 2)
 
-  def square(self):
-    a = self.position
-    b = self.position + pygame.Vector2(self.position[0] + PLAYER_WIDTH, self.position[1])
-    c = self.position + pygame.Vector2(self.position[0] + PLAYER_WIDTH, self.position[1] + PLAYER_WIDTH)
-    d = self.position + pygame.Vector2(self.position[0], self.position[1] + PLAYER_WIDTH)
-    return [a, b, c, d]
+class Player(SquareShape):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.__speed = pygame.Vector2(0, 0)
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            self.__speed = pygame.Vector2(0, INITIAL_PLAYER_SPEED * -1)
+        if keys[pygame.K_DOWN]:
+            self.__speed = pygame.Vector2(0, INITIAL_PLAYER_SPEED)
+        if keys[pygame.K_LEFT]:
+            self.__speed = pygame.Vector2(INITIAL_PLAYER_SPEED * -1, 0)
+        if keys[pygame.K_RIGHT]:
+            self.__speed = pygame.Vector2(INITIAL_PLAYER_SPEED, 0)
+        self.position += self.__speed * dt
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, (0, 0, 0), self.square())
+
+    def square(self):
+        return pygame.Rect(
+            self.position[0],
+            self.position[1],
+            PLAYER_WIDTH,
+            PLAYER_WIDTH)
+
+    def collides_with_border(self, width, height):
+        return self.position[1] <= 0 \
+            or self.position[1] + PLAYER_WIDTH >= height \
+            or self.position[0] <= 0 \
+            or self.position[0] + PLAYER_WIDTH >= width
